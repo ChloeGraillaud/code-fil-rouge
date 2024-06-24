2ec654b390ecea9ecaa3aefb453ec53fe140720b
ChloeGraillaud <cgraillaud@laposte.net>
2024-06-24 11:37:44 +0200

    Add testimonies carousel

--- script.js
+++ script.js
@@ -54,4 +54,35 @@ document.addEventListener('DOMContentLoaded', function() {
         });
         
         
+// Carousel de témoignage
+$(document).ready(function(){
+    $('.testimonial-carousel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
+
+
         
